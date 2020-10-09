9e29f24460f7636dc00e9d2e1204b1d3ca4c9fdb
Odis Barnett <kingo.iv@gmail.com>
2020-10-09 13:09:21 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     return (
         <Router>
            <Switch>
-                <Route exact path='/' component={Home} />
+                <Route exact path='' component={Home} />
                 <Route path='/signin' component={SigninPage} exact/>
            </Switch>
         </Router>
