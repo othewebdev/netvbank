9a1171142bad51a1c171d6a07bbc202eca26a52e
Odis Barnett <kingo.iv@gmail.com>
2020-10-09 12:17:06 -0500

    Updates

---
+++ precache-manifest.b83873aca57f02d1acda87976c1a4036.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9912e04f6525cc40e16ca6801b44ca8",
+    "url": "/netvink/index.html"
+  },
+  {
+    "revision": "8d42ad19236adccac354",
+    "url": "/netvink/static/css/main.7b6cae3b.chunk.css"
+  },
+  {
+    "revision": "d05041e268d9be663933",
+    "url": "/netvink/static/js/2.a86ccf8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netvink/static/js/2.a86ccf8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d42ad19236adccac354",
+    "url": "/netvink/static/js/main.c24d5785.chunk.js"
+  },
+  {
+    "revision": "6235a77f3e47582534a2",
+    "url": "/netvink/static/js/runtime-main.d9c0b9f9.js"
+  },
+  {
+    "revision": "e97df77b1767e165ababc7db4ed672d2",
+    "url": "/netvink/static/media/svg-1.e97df77b.svg"
+  },
+  {
+    "revision": "735686b1d75b1fc8ec69f84a70e260fa",
+    "url": "/netvink/static/media/svg-2.735686b1.svg"
+  },
+  {
+    "revision": "967a922f8624b7a0adf3a3c2a4073144",
+    "url": "/netvink/static/media/svg-3.967a922f.svg"
+  },
+  {
+    "revision": "c83bb5ad02aa1381c97a700ccd23e2df",
+    "url": "/netvink/static/media/svg-4.c83bb5ad.svg"
+  },
+  {
+    "revision": "0e7d587865cd72cee34505e3e892f118",
+    "url": "/netvink/static/media/svg-phone.0e7d5878.svg"
+  },
+  {
+    "revision": "f44d422136d941d884b93dd2d3c7e0c6",
+    "url": "/netvink/static/media/svg-rewards.f44d4221.svg"
+  },
+  {
+    "revision": "d0f3fa1ec0a56fdef821bd1f9b2fe325",
+    "url": "/netvink/static/media/svg-track.d0f3fa1e.svg"
+  },
+  {
+    "revision": "068b1c3456f23d56ea00acdb6893004b",
+    "url": "/netvink/static/media/svg-world.068b1c34.svg"
+  },
+  {
+    "revision": "0e7ef2c1e73097bb5cc103f8bebe5128",
+    "url": "/netvink/static/media/video.0e7ef2c1.mp4"
+  }
+]);
